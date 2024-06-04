03329b07bbca78e282088ffcdf6a265d92370b6e
Irina.Voron <yarutich.irina@gmail.com>
2024-06-04 13:56:14 +0300

    Add Browser and Config classes

--- seleyasha/browser.py
+++ seleyasha/browser.py
@@ -5,45 +5,48 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.expected_conditions import _element_if_visible
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from dataclasses import dataclass
 
 from seleyasha.selector import to_locator
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-wait = WebDriverWait(driver, timeout=10, ignored_exceptions=(WebDriverException,))
+@dataclass
+class Config:
+    timeout = 5
 
 
-def assert_(condition):
-    return wait.until(condition)
+class Browser:
+    def __init__(self, driver, config=Config()):
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.config = config
+        self.wait = WebDriverWait(driver, timeout=config.timeout, ignored_exceptions=(WebDriverException,))
 
+    def assert_(self, condition):
+        return self.wait.until(condition)
 
-def open(url):
-    driver.get(url)
+    def open(self, url):
+        self.driver.get(url)
 
+    def quit(self):
+        self.driver.quit()
 
-def quit():
-    driver.quit()
+    def element(self, selector):
+        def command(driver):
+            return _element_if_visible(driver.find_element(*to_locator(selector)))
 
+        return self.wait.until(command, message=f'element by {selector} isn`t ready')
 
-def element(selector):
-    def command(driver):
-        return _element_if_visible(driver.find_element(*to_locator(selector)))
+    def type_to_element(self, selector, value):
+        def predicate(driver: WebDriver):
+            webelement = driver.find_element(*to_locator(selector))
+            # driver.execute_script()
+            webelement.send_keys(value)
+            return True
 
-    return wait.until(command, message=f'element by {selector} isn`t ready')
+        return self.wait.until(predicate, message=f'failed to type into element by {selector}')
 
+    def click_element(self, selector):
+        def command(driver: WebDriver):
+            driver.find_element(*to_locator(selector)).click()
+            return True
 
-def type_to_element(selector, value):
-    def predicate(driver: WebDriver):
-        webelement = driver.find_element(*to_locator(selector))
-        # driver.execute_script()
-        webelement.send_keys(value)
-        return True
-
-    return wait.until(predicate, message=f'failed to type into element by {selector}')
-
-
-def click_element(selector):
-    def command(driver: WebDriver):
-        driver.find_element(*to_locator(selector)).click()
-        return True
-
-    return wait.until(command, message=f'failed to click element by {selector}')
+        return self.wait.until(command, message=f'failed to click element by {selector}')
--- tests/test_google_custom.py
+++ tests/test_google_custom.py
@@ -1,14 +1,17 @@
 from selenium.webdriver import Keys
-
+from selenium import webdriver
 from seleyasha import match
 from seleyasha import browser
-from seleyasha.browser import assert_, element, type_to_element, click_element
+from seleyasha.browser import Browser
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
+browser = Browser(webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())))
 browser.open('https://www.ecosia.org/')
 
-type_to_element('[name=q]', value='releases selene yashaka' + Keys.ENTER)
-element('[data-test-id=mainline-result-web]:nth-of-type(1) a').click()
-click_element('[id=pull-requests-tab]')
-assert_(match.number_of_elements('a[id^="issue_"]', 0))
+browser.type_to_element('[name=q]', value='releases selene yashaka' + Keys.ENTER)
+browser.element('[data-test-id=mainline-result-web]:nth-of-type(1) a').click()
+browser.click_element('[id=pull-requests-tab]')
+browser.assert_(match.number_of_elements('a[id^="issue_"]', 0))
 
 browser.quit()
